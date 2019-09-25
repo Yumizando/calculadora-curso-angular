eb0826a99ed922a8f702586ae1b33c1d25e2128e
Laryssa Yumi <laryyumi@gmail.com>
2019-09-25 15:52:49 -0300

    Calculadora.service documentado

--- src/app/calculadora/services/calculadora.service.ts
+++ src/app/calculadora/services/calculadora.service.ts
@@ -1,3 +1,10 @@
+/** 
+ * Serviço responsável por executar as operações 
+ * da calculadora.
+ * 
+ * @author Laryssa Yumi Nacasima Barros 
+ * @since 1.0.0
+*/
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,15 +12,26 @@ import { Injectable } from '@angular/core';
 })
 export class CalculadoraService {
 
+  /* Define as constantes utilizadas para
+     identificar as operações de cálculo*/ 
   static readonly SOMA: string = '+';
   static readonly SUBTRACAO: string = '-';
   static readonly DIVISAO: string = '/';
   static readonly MULTIPLICACAO: string = '*';
 
   constructor() { }
-
+  /**
+   * Método que calcula uma operação matemática
+   * dado dois números.
+   * Suporta as operações soma, subtração, divisão e multiplicação.
+   * 
+   * @param num1 number 
+   * @param num2 number
+   * @param operacao string Operação a ser executada
+   * @return number Resultado da operação
+   */
   calcular(num1: number, num2: number, operacao: string): number{
-    let resultado: number;
+    let resultado: number; //armazena o resultado da documentação
 
     switch (operacao) {
       case CalculadoraService.SOMA:
@@ -32,7 +50,7 @@ export class CalculadoraService {
         resultado = 0;
         break;
     }
-    
+
     return resultado;
   }
 
